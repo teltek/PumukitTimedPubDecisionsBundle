8a0fdd92afe6db16abf4f19f3b2bf172878051be
Alfonso Rodríguez Pereira <arodriguez@teltek.es>
2020-04-23 19:25:06 +0200

    Added typing

--- Services/MenuService.php
+++ Services/MenuService.php
@@ -6,17 +6,17 @@ use Pumukit\NewAdminBundle\Menu\ItemInterface;
 
 class MenuService implements ItemInterface
 {
-    public function getName()
+    public function getName(): string
     {
         return 'Timed Publishing Decisions Timeframes';
     }
 
-    public function getUri()
+    public function getUri(): string
     {
         return 'pumukit_newadmin_timeframes_index_default';
     }
 
-    public function getAccessRole()
+    public function getAccessRole(): string
     {
         return 'ROLE_ACCESS_TIMEDPUBDECISIONS_TIMELINE';
     }
